dd63c4eac9aadf3d4b470825b611b9e9463d5a7d
Daniel Sângelo <danielsangelo@hotmail.com>
2022-05-28 11:07:39 -0300

    Using throw new to return an exception

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,10 @@
 import {
+	BadRequestException,
 	forwardRef,
-	HttpException,
-	HttpStatus,
 	Inject,
 	Injectable,
 	NotFoundException,
+	UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FindConditions, FindOneOptions, Repository } from 'typeorm';
@@ -20,20 +20,17 @@ export class UserService {
 		@InjectRepository(User)
 		private readonly userRepository: Repository<User>,
 		@Inject(forwardRef(() => AuthService))
-		private readonly authService: AuthService
+		private readonly authService: AuthService,
 	) {}
 
 	async create(createUserDto: CreateUserDto) {
 		const userExists = await this.findByEmail(createUserDto.email);
 
-		if (userExists)
-			throw new HttpException(
-				'User already exist',
-				HttpStatus.BAD_REQUEST,
-			);
+		if (userExists) throw new BadRequestException('User already exist');
 
 		const user = this.userRepository.create(createUserDto);
 		await this.userRepository.save(user);
+
 		return;
 	}
 
@@ -46,45 +43,40 @@ export class UserService {
 	async findByEmail(email: string): Promise<User> {
 		const user = await this.userRepository.findOne({ email });
 		if (!user) throw new NotFoundException();
+
 		return user;
 	}
-
-	// see later
-	// async findOneOrFail(
-	// 	conditions: FindConditions<User>,
-	// 	options?: FindOneOptions<User>,
-	// ) {
-	// 	try {
-	// 		return this.userRepository.findOneOrFail(conditions, options);
-	// 	} catch (error) {
-	// 		throw new NotFoundException(error.message);
-	// 	}
-	// }
 
 	async findOne(id: number) {
 		const user = await this.userRepository.findOne(id);
 		if (!user) throw new NotFoundException(`User not found`);
+
 		return;
 	}
 
 	async update(id: number, updateUserDto: UpdateUserDto) {
 		const user = await this.userRepository.findOne(id);
 		if (!user) throw new NotFoundException(`User not found`);
+
 		updateUserDto.updatedAt = this.dtNow();
 		updateUserDto.password = genHash.generateHash(
 			updateUserDto.password,
 			10,
 		);
+
 		this.userRepository.merge(user, updateUserDto);
 		await this.userRepository.save(user);
+
 		return;
 	}
 
 	async remove(id: number) {
 		const user = await this.userRepository.findOne(id);
 		if (!user) throw new NotFoundException(`User not found`);
+
 		user.deletedAt = this.dtNow();
 		await this.userRepository.save(user);
+
 		return;
 	}
 
@@ -106,14 +98,9 @@ export class UserService {
 
 	async refreshToken(oldToken: string) {
 		let user = await this.userRepository.findOne({ token: oldToken });
-		if (user) {
-			return this.authService.login(user);
-		}
-		return new HttpException(
-			{
-				errormessage: 'Invalid token',
-			},
-			HttpStatus.UNAUTHORIZED,
-		);
+
+		if (!user) throw new UnauthorizedException('Invalid token');
+
+		return this.authService.login(user);
 	}
 }
